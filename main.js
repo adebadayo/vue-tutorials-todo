65809039af38e9230ddd177695a7adc3f545e4fa
adebadayo <adebayanen@gmail.com>
2021-11-24 22:18:52 +0900

    コピペ

--- main.js
+++ main.js
@@ -1,31 +1,79 @@
+// ★STEP2
 // https://jp.vuejs.org/v2/examples/todomvc.html
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
-    fetch: function() {
-        var todos = JSON.parse(
-          localStorage.getItem(STORAGE_KEY) || '[]'
-        )
-        todos.forEach(function(todo, index) {
+    fetch: function () {
+        var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+        todos.forEach(function (todo, index) {
             todo.id = index
         })
         todoStorage.uid = todos.length
         return todos
     },
-    save: function(todos) {
+    save: function (todos) {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
     }
 }
 
-// コンストラクタ関数 Vue を使ってルートインスタンスを作成します。
-// アプリケーションで使用したいデータは data オプションへ登録していきます。
 
-const app = new Vue({
+// ★STEP1
+new Vue({
     el: '#app',
+
     data: {
-        todos: []
+        // ★STEP5 localStorage から 取得した ToDo のリスト
+        todos: [],
+        // ★STEP11 抽出しているToDoの状態
+        current: -1,
+        // ★STEP11＆STEP13 各状態のラベル
+        options: [
+            { value: -1, label: 'すべて' },
+            { value: 0, label: '作業中' },
+            { value: 1, label: '完了' }
+        ]
     },
+
+    computed: {
+
+        // ★STEP12
+        computedTodos: function () {
+            return this.todos.filter(function (el) {
+                return this.current < 0 ? true : this.current === el.state
+            }, this)
+        },
+
+        // ★STEP13 作業中・完了のラベルを表示する
+        labels() {
+            return this.options.reduce(function (a, b) {
+                return Object.assign(a, { [b.value]: b.label })
+            }, {})
+            // キーから見つけやすいように、次のように加工したデータを作成
+            // {0: '作業中', 1: '完了', -1: 'すべて'}
+        }
+    },
+
+    // ★STEP8
+    watch: {
+        // オプションを使う場合はオブジェクト形式にする
+        todos: {
+            // 引数はウォッチしているプロパティの変更後の値
+            handler: function (todos) {
+                todoStorage.save(todos)
+            },
+            // deep オプションでネストしているデータも監視できる
+            deep: true
+        }
+    },
+
+    // ★STEP9
+    created() {
+        // インスタンス作成時に自動的に fetch() する
+        this.todos = todoStorage.fetch()
+    },
+
     methods: {
-        // ToDo 追加の処理
+
+        // ★STEP7 ToDo 追加の処理
         doAdd: function(event, value) {
             // ref で名前を付けておいた要素を参照
             var comment = this.$refs.comment
@@ -44,28 +92,17 @@ const app = new Vue({
             // フォーム要素を空にする
             comment.value = ''
         },
-        doChangeState: function(item) {
-            item.state = item.state ? 0 : 1
+
+        // ★STEP10 状態変更の処理
+        doChangeState: function (item) {
+            item.state = !item.state ? 1 : 0
         },
-        // 削除の処理
-        doRemove: function(item) {
+
+        // ★STEP10 削除の処理
+        doRemove: function (item) {
             var index = this.todos.indexOf(item)
             this.todos.splice(index, 1)
         }
-    },
-    watch: {
-        // オプションを使う場合はオブジェクト形式にする
-        todos: {
-            // 引数はウォッチしているプロパティの変更後の値
-            handler: function(todos) {
-                todoStorage.save(todos)
-            },
-            // deep オプションでネストしているデータも監視できる
-            deep: true
-        }
-    },
-    created() {
-        // インスタンス作成時に自動的に fetch() する
-        this.todos = todoStorage.fetch()
+
     }
 })
